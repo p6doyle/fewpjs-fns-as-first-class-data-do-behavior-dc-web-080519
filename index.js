2b68fb2c9958ad6b3af4426cfae6c9ae572d90d6
p6doyle <p6doyle@gmail.com>
2019-09-15 14:56:09 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const hour = parseInt(string, 10);
+  if (hour < 12) return "Good Morning"
+  if (hour > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+//
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
